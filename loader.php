106fbb88122377f452f13e83ec8da104cbe61a32
Taha Amin Ghafuri <82469904+tahaghafuri@users.noreply.github.com>
2023-03-04 16:55:39 +0330

    Fix Loader Bug

--- loader.php
+++ loader.php
@@ -6,12 +6,13 @@
 
 $dir = __DIR__.'/src/';
 
+require_once $dir.'UserManager.php';
 require_once $dir.'Administration.php';
+require_once $dir.'AuthException.php';
 require_once $dir.'AmbiguousUsernameException.php';
 require_once $dir.'AttemptCancelledException.php';
 require_once $dir.'Auth.php';
 require_once $dir.'AuthError.php';
-require_once $dir.'AuthException.php';
 require_once $dir.'ConfirmationRequestNotFound.php';
 require_once $dir.'DatabaseError.php';
 require_once $dir.'DuplicateUsernameException.php';
@@ -31,6 +32,5 @@ require_once $dir.'TooManyRequestsException.php';
 require_once $dir.'UnknownIdException.php';
 require_once $dir.'UnknownUsernameException.php';
 require_once $dir.'UserAlreadyExistsException.php';
-require_once $dir.'UserManager.php';
 
 ?>
